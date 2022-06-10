cd59259064a9af115f3ab375827217f40d391717
Anthony <anthonymarques@outlook.com>
2022-06-10 12:35:53 +0200

    setting up component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { DatatableComponent } from '@swimlane/ngx-datatable';
 import { PaginationModel } from './models/paringation.model';
 
@@ -7,7 +7,7 @@ import { PaginationModel } from './models/paringation.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'datatable-pager-sample';
 
   @ViewChild(DatatableComponent) table: DatatableComponent;
@@ -20,11 +20,54 @@ export class AppComponent {
   public pagination: PaginationModel;
   public currentPage: number = 1;
   public offset: number = 1;
+  public pageSize:number = 10;
 
   // PageChange values
   public pageSizeLimits: number[] = [5, 10, 20, 50];
 
+  // Fake API response
+  public totalSearchResults:number = 0;
 
+  ngOnInit() {
+    this.initDatatableColumns();
+  }
 
+private setPagination(): void {
+  this.pagination = {
+    totalItems: this.totalSearchResults,
+    pageSize: this.pageSize,
+    activePage: this.currentPage,
+  }
+
+}
+
+  public initDatatableColumns(): void {
+    this.columns = [
+      {
+        prop: 'firstName',
+        name: 'First name',
+        sortable: false,
+        draggable: false,
+      },
+      {
+        prop: 'lastName',
+        name: 'Last name',
+        sortable: false,
+        draggable: false,
+      },
+      {
+        prop: 'age',
+        name: 'age',
+        sortable: false,
+        draggable: false,
+      },
+      {
+        prop: 'location',
+        name: 'Area name',
+        sortable: false,
+        draggable: false,
+      },
+    ]
+  }
 
 }

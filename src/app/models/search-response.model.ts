a2748fd53820d7083c8470166c4efe9a07f44314
Anthony <anthonymarques@outlook.com>
2022-06-21 11:07:55 +0200

    updating modal

--- src/app/models/search-response.model.ts
+++ src/app/models/search-response.model.ts
@@ -1,6 +1,19 @@
 export class SearchResults {
+    id: number;
     firstName: string;
     lastName: string;
-    age: number;
+    email: string;
     location: string;
 }
+
+"id": 3,
+"firstName": "Mary",
+"lastName": "Marques",
+"email": "mary@gmail.com",
+"gender": "female",
+"location": "CH"
+
+"id": 4,  
+"name": "Smith",  
+"email": "smith@gmail.com",  
+"gender": "male"  
